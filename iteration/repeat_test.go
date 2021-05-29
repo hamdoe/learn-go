be5bf9279cd483b0d6f8863d48ef15e6ef39031e
hamdoe <hjh940922@gmail.com>
2021-05-29 21:30:27 +0900

    iteration 리팩토링

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,12 +1,36 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 10)
+	expected := "aaaaaaaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 5)
+	fmt.Println(repeated)
+	// Output: aaaaa
+}
+
+func TestTitle(t *testing.T) {
+	splited := strings.Title("hello world")
+	expected := "Hello World"
+	if splited != expected {
+		t.Errorf("expected %q but got %q", splited, expected)
 	}
 }
